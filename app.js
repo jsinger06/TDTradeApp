bdfa4d7a135c34163d2c794abb62b07e949e1e45
Josh Singer <joshua.j.singer79@gmail.com>
2019-11-15 07:14:22 -0500

    configure bin/www to start the application

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
-const fs = require('fs');
-const https = require('https');
 const express = require('express');
-const mongoose = require('mongoose');
 const oAuthToken = require('./routes/auth');
 const quotes = require('./routes/quotes');
-const config = require('./config');
-const account = require('./routes/account');
-
-//Load SSL certificate and private key from files
-const privateKey  = fs.readFileSync('./cert/key.pem', 'utf8');
-const certificate = fs.readFileSync('./cert/certificate.pem', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
-
-mongoose.Promise = global.Promise;
-mongoose.connect(config.mongodb)
-    .then(() => {
-      console.log('Connected to DB');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
 
 const app = express();
 
-// Start a secure web server and listen on port 8443
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(config.PORT);
-console.log("Listening on port 8443...");
-
-//load access token on start up
-oAuthToken.initializeTokens();
+/*todo: figure out where to load oAuthToken
+load access token on start up
+oAuthToken.initializeTokens();*/
 
 // redirect uri
 app.get('/auth/newtoken', (req, res) => {
   console.log('New request');
-  oAuthToken.getToken(req.query.code);
-  res.end();
+  oAuthToken.getToken(req.query.code)
+      .then(() => res.end())
+      .catch(err => res.end(err))
 });
 
 // Manually force token renewal
@@ -52,4 +28,4 @@ app.get('/quote/', (req, res) => {
     quotes.requestQuote(req.query.symbolList, res);
 });
 
-setTimeout(account.getAccount, 3000);
+module.exports = app;
