317620be9b9606fa45239a1725f71e3bbef10c4d
Josh Singer <joshua.j.singer79@gmail.com>
2019-11-19 07:07:20 -0500

    modified auth functions to return promises for asynch functions for getToken to work properly

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -29,31 +29,42 @@ const authToken = {
 /**
  * Function for requesting a new or refreshed token
  * Stores the returned Access and Refresh tokens in mongo db
-*/
+ * @returns {Promise<Object>}
+ */
 const requestToken = async () => {
+    debug(`start requestToken`);
     let data = querystring.stringify(options.data);
 
-    axios.post('https://api.tdameritrade.com/v1/oauth2/token',
-        data, {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        })
-        .then( async (oAuthReply) => {
-            let currentTime = Date.now();
+    return new Promise((resolve, reject) => {
+        axios.post('https://api.tdameritrade.com/v1/oauth2/token',
+            data, {
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            })
+            .then(async (oAuthReply) => {
+                debug(`cache and store token`);
+                let currentTime = Date.now();
 
-            authToken.updatedAt = currentTime;
-            authToken.access_token = 'Bearer ' + oAuthReply.data.access_token;
+                authToken.updatedAt = currentTime;
+                authToken.access_token = 'Bearer ' + oAuthReply.data.access_token;
 
-            let writeDB = {
-                "_id": '1',
-                "access_token": oAuthReply.data.access_token,
-                "refresh_token": oAuthReply.data.refresh_token,
-                'updatedAt': currentTime
-            };
+                let writeDB = {
+                    "_id": '1',
+                    "access_token": oAuthReply.data.access_token,
+                    "refresh_token": oAuthReply.data.refresh_token,
+                    'updatedAt': currentTime
+                };
 
-            await tdauths.findOneAndUpdate({"_id":"1"}, writeDB, { new: true, upsert: true});
-        })
-        .then( () => console.log(`Token updated in DB`))
-        .catch( (err) => console.log(err));
+                await tdauths.findOneAndUpdate({"_id": "1"}, writeDB, {new: true, upsert: true});
+            })
+            .then(() => {
+                console.log(`Token updated in DB`);
+                resolve(authToken);
+            })
+            .catch((err) => {
+                console.log(err);
+                reject(err);
+            })
+    })
 };
 
 const lookupToken = () => {
@@ -76,24 +87,27 @@ const initializeTokens = () => {
 
 /**
  * Return a fresh token
- *
+ * @returns {Promise<accessToken>}
  */
 const getToken = () => {
+
     return new Promise((resolve, reject) => {
         module.exports.validateTokenIsFresh()
-            .then( async (isTokenFresh) => {
-                if( isTokenFresh ){
+            .then((isTokenFresh) => {
+                if (isTokenFresh) {
                     debug(`Access Token: ${authToken.access_token}`);
-                    resolve(authToken.access_token)
+                    resolve(authToken.access_token);
                 } else {
-                    console.log('refreshing token');
-                    await module.exports.refreshToken();
-                    resolve(authToken.access_token)
+                    module.exports.refreshToken()
+                        .then(() => {
+                            debug(`finishing getToken - ${authToken.access_token}`);
+                            resolve(authToken.access_token);
+                        });
                 }
             })
             .catch(() => {
                 debug(`Token was invalid`);
-                reject('Error getting token')
+                reject('Error getting token');
             });
     });
 };
@@ -110,18 +124,28 @@ const newAccessToken = async (oAuthCode) => {
 
 /**
  * Builds request structure for requesting a new token using a refresh token
+ * @returns {Promise<void>}
  */
 const refreshToken = () => {
-    tdauths.find({"_id": "1"}, 'refresh_token')
-        .then( (queryResult) => {
-            options.data.grant_type = 'refresh_token';
-            options.data.refresh_token = queryResult[0].refresh_token;
-            options.data.code = '';
-            requestToken()
-                .then( () =>  console.log(`Token Refreshed`) )
-                .catch( (err) =>  console.log(err) )
-        })
-        .catch( (err) => console.log(err) );
+    debug(`start refreshToken`);
+    return new Promise((resolve, reject) => {
+        tdauths.find({"_id": "1"}, 'refresh_token')
+            .then((queryResult) => {
+                options.data.grant_type = 'refresh_token';
+                options.data.refresh_token = queryResult[0].refresh_token;
+                options.data.code = '';
+                requestToken()
+                    .then(() => {
+                        debug('token refreshed');
+                        resolve();
+                    })
+                    .catch((err) => console.log(err))
+            })
+            .catch((err) => {
+                console.log(err);
+                reject(err);
+            });
+    })
 };
 
 /**
@@ -129,7 +153,7 @@ const refreshToken = () => {
  * @returns {Promise<Boolean>}
  */
 const validateTokenIsFresh = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         if (authToken.updatedAt === undefined || authToken.updatedAt === null) {
             return resolve(false);
         }
--- routes/quotes/index.js
+++ routes/quotes/index.js
@@ -1,6 +1,7 @@
 const axios = require ('axios');
 const {authToken} = require('../auth');
 const config = require('../../config');
+const debug = require('debug')('tradingapi:routes:quotes');
 
 const requestQuote = (symbols, res) => {
     axios
@@ -26,4 +27,27 @@ const requestQuote = (symbols, res) => {
         });
 };
 
-module.exports = {requestQuote};
+const requestQuoteTemp = async (token,symbols) => {
+    debug(`start requestQuoteTemp`);
+    await axios
+        .request({
+            url: 'https://api.tdameritrade.com/v1/marketdata/quotes',
+            method: 'GET',
+            headers: {
+                'Authorization': token,
+            },
+            params: {
+                apikey: config.acct.apikey,
+                symbol: symbols //symbol hardcoded for testing - use symbols parameter
+            }
+        })
+        .then((tdResponse) => {
+            console.log(tdResponse.data);
+        })
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
+};
+
+module.exports = {requestQuote, requestQuoteTemp};
