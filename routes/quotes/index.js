c204031eb03c712f38c049b6e81e66229d40cdc6
Josh Singer <joshua.j.singer79@gmail.com>
2019-11-20 07:22:25 -0500

    fix sandbox code that was committed

--- routes/quotes/index.js
+++ routes/quotes/index.js
@@ -1,53 +1,32 @@
 const axios = require ('axios');
-const {authToken} = require('../auth');
 const config = require('../../config');
 const debug = require('debug')('tradingapi:routes:quotes');
 
-const requestQuote = (symbols, res) => {
-    axios
-        .request({
-            url: 'https://api.tdameritrade.com/v1/marketdata/quotes',
-            method: 'GET',
-            headers: {
-                'Authorization': authToken.access_token,
-            },
-            params: {
-                apikey: config.acct.apikey,
-                symbol: symbols //symbol hardcoded for testing - use symbols parameter
-            }
-        })
-        .then( (tdResponse) => {
-            console.log(tdResponse.data);
-            res.json(tdResponse.data);
-        })
-        .catch( (err) => {
-            console.log(err);
-            res.send(`Error Encountered: ${err}`);
-            throw err;
-        });
-};
 
-const requestQuoteTemp = async (token,symbols) => {
+const requestQuote = (token,symbols) => {
     debug(`start requestQuoteTemp`);
-    await axios
-        .request({
-            url: 'https://api.tdameritrade.com/v1/marketdata/quotes',
-            method: 'GET',
-            headers: {
-                'Authorization': token,
-            },
-            params: {
-                apikey: config.acct.apikey,
-                symbol: symbols //symbol hardcoded for testing - use symbols parameter
-            }
-        })
-        .then((tdResponse) => {
-            console.log(tdResponse.data);
-        })
-        .catch((err) => {
-            console.log(err);
-            throw err;
-        });
+    return new Promise((resolve, reject) => {
+        axios
+            .request({
+                url: 'https://api.tdameritrade.com/v1/marketdata/quotes',
+                method: 'GET',
+                headers: {
+                    'Authorization': token,
+                },
+                params: {
+                    apikey: config.acct.apikey,
+                    symbol: symbols //symbol hardcoded for testing - use symbols parameter
+                }
+            })
+            .then((tdResponse) => {
+                console.log(tdResponse.data);
+                resolve(tdResponse.data);
+            })
+            .catch((err) => {
+                console.log(err);
+                reject(err);
+            });
+    });
 };
 
-module.exports = {requestQuote, requestQuoteTemp};
+module.exports = {requestQuote};
